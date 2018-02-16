f8f1456ac766a5b91581951d9f57ed6edbb17976
rookalkar <rookalkar@gmail.com>
2018-02-16 12:15:30 -0500

    Minor changes with code separation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-window.onload = function() {
-    document.getElementsByTagName("button").onclick = function() {
-        alert("hello");
-    }
-    alert("hi");
+function submit() {
+    var payload = {};
+    payload.element = document.getElementById("element").value;
+    payload.attr = document.getElementById("attr").value;
+    payload.dim = document.getElementById("dim").value;
+    payload.value = document.getElementById("value").value;
+    change(payload);
+}
+
+function change (payload) {
+  var current_attr_value = {};
+  current_attr_value = document.getElementById(payload.element).getAttribute(payload.attr);
+  current_attr_value[payload.dim] = current_attr_value[payload.dim] + parseFloat(payload.value);
+  document.getElementById(payload.element).setAttribute(payload.attr, current_attr_value);
 }
--- js/swipe.js
+++ js/swipe.js
@@ -6,13 +6,13 @@ var touchendY = 0;
 var gesturedZone = document.getElementById('gesturedZone');
 
 gesturedZone.addEventListener('touchstart', function(event) {
-    console.log('start');
+    //console.log('start');
     touchstartX = event.changedTouches[0].screenX;
     touchstartY = event.changedTouches[0].screenY;
 }, false);
 
 gesturedZone.addEventListener('touchend', function(event) {
-    console.log('end');
+    //console.log('end');
     touchendX = event.changedTouches[0].screenX;
     touchendY = event.changedTouches[0].screenY;
     handleGesure();
@@ -20,23 +20,23 @@ gesturedZone.addEventListener('touchend', function(event) {
 
 function handleGesure() {
   if (touchendX <= touchstartX) {
-      alert('Swiped left');
+    //  alert('Swiped left');
       document.getElementById('object').style.display = 'none';
       document.getElementById('camera').style.display = 'block';
   }
 
   if (touchendX >= touchstartX) {
-      alert('Swiped right');
+    //  alert('Swiped right');
       document.getElementById('object').style.display = 'block';
       document.getElementById('camera').style.display = 'none';
   }
 
   if (touchendY <= touchstartY) {
-      alert('Swiped up');
+    //  alert('Swiped up');
   }
 
   if (touchendY >= touchstartY) {
-      alert('Swiped down');
+    //  alert('Swiped down');
   }
 
   if (touchendY === touchstartY) {
